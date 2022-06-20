cb96e56c3ee88055adceaae4a79828ad22cfee3c
Dariusz Okonski <darek200180@gmail.com>
2022-06-20 19:15:42 +0100

    install gh-pages

--- src/data/EventsList.ts
+++ src/data/EventsList.ts
@@ -6,6 +6,6 @@ export interface Event {
 }
 
 export const eventsList = ():Event[] => [
-    {id: 1, title: 'first event', date: '20-01-1980', city: 'manchester'}, 
-    {id: 2, title: 'second event', date: '20-01-1980', city: 'manchester'}, 
-    {id: 3, title: 'third event', date: '20-01-1980', city: 'manchester'}]
+    {id: 1, title: 'first event', date: '20-03-2022', city: 'manchester'}, 
+    {id: 2, title: 'second event', date: '12-01-2021', city: 'london'}, 
+    {id: 3, title: 'third event', date: '20-01-2022', city: 'manchester'}]
